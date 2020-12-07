32a41003014a46d9e26d049f6697d2d016ce6b95
Andres Becker <abecker.esfm@gmail.com>
2020-12-07 23:24:43 +0100

    restnet added to try to use one pretrained

--- workspace/libs/Models.py
+++ workspace/libs/Models.py
@@ -43,6 +43,9 @@ class Predef_models():
         elif self.model_name == 'ResNet50V2':
             self.model = self._get_ResNet50V2()
 
+        elif self.model_name == 'ResNet50V2_PreTrained':
+            self.model = self._get_ResNet50V2_PreTrained()
+
         else:
             msg = 'Specified model {} not implemented!'.format(self.model_name)
             self.log.error(msg)
@@ -199,6 +202,34 @@ class Predef_models():
 
         x = base_model.output
         x = tf.keras.layers.GlobalAveragePooling2D()(x)
+        prediction = tf.keras.layers.Dense(1)(x)
+
+        model = tf.keras.models.Model(inputs=base_model.inputs, outputs=prediction)
+
+        return model
+
+    def _get_ResNet50V2_PreTrained(self):
+
+        input_layer = tf.keras.Input(shape=self.input_shape,
+            #batch_size=p['BATCH_SIZE'],
+            name='InputLayer')
+
+        base_model = tf.keras.applications.ResNet50V2(
+            include_top=False,
+            #weights="imagenet",
+            weights=None,
+            input_tensor=input_layer,
+            #input_shape=None,
+            pooling=None,
+            #classes=1000,
+            classifier_activation=None,
+            #classifier_activation='softmax',
+            )
+
+        x = base_model.output
+        x = tf.keras.layers.Flatten()(x)
+        x = tf.keras.layers.Dense(512)(x)
+        x = tf.keras.layers.Dense(128)(x)
         prediction = tf.keras.layers.Dense(1)(x)
 
         model = tf.keras.models.Model(inputs=base_model.inputs, outputs=prediction)
