af4c5445771962433fb3b12b4f98a9a6bc0427aa
Andres Becker <abecker.esfm@gmail.com>
2020-12-12 22:10:42 +0100

    plot functions added

--- workspace/libs/Models.py
+++ workspace/libs/Models.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
 import logging
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme(style="darkgrid")
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import tensorflow as tf
+import pandas as pd
 
 class Predef_models():
     """
@@ -312,3 +318,131 @@ class Predef_models():
         model = tf.keras.models.Model(inputs=base_model.inputs, outputs=prediction)
 
         return model
+
+def plot_error_dist(df, y_models):
+    temp_df = df.copy()
+    diff_names = ['set', 'perturbation']
+    for y_model in y_models:
+        # Column name for difference between y and y_model
+        col_name = 'y - '+y_model
+        diff_names.append(col_name)
+        temp_df[col_name] = temp_df['y'] - temp_df[y_model]
+
+    temp_df = temp_df[diff_names].set_index(['set', 'perturbation'])
+    temp_df = temp_df.stack().reset_index()
+    #temp_df = temp_df.drop(['level_0'], axis=1)
+    temp_df.columns = ['set', 'perturbation'] + ['diff_name', 'value']
+
+    plt.figure(figsize=(20,7))
+    sns.kdeplot(x='value',
+                data=temp_df,
+                hue='diff_name',
+                #color=colors,
+                shade=True,
+                bw_method=0.2)
+
+    plt.xlabel('y - y_hat')
+    plt.title('Error KDE per model')
+
+    plt.figure(figsize=(len(y_models)*7,7))
+    sns.boxplot(y='value',
+                x='diff_name',
+                hue='perturbation',
+                data=temp_df)
+    plt.xlabel('Diff Name')
+    plt.ylabel('y - y_hat')
+    plt.title('Error Distribution per set')
+
+    temp_df = df.loc[:, df.columns != 'perturbation']
+    temp_df = temp_df.set_index(['mapobject_id_cell', 'set']).stack().reset_index()
+    temp_df.columns = ['mapobject_id_cell', 'set', 'var', 'value']
+
+    plt.figure(figsize=(15,7))
+    sns.boxplot(y='value',
+                x='var',
+                hue='set',
+                data=temp_df)
+    plt.title('Transcription Rate (TR) values distribution')
+
+def plot_residuals(df, y_hat):
+
+    df['diff'] = df['y'] - df[y_hat]
+    std = df['diff'].std()
+    df['std_residuals'] = df['diff'] / std
+
+    sns.scatterplot(
+        data=df,
+        x = 'y',
+        y = 'std_residuals',
+        hue = 'perturbation',
+    )
+    plt.hlines(y=2, xmin=np.min(df['y']), xmax=np.max(df['y']), color='red', ls='dashed')
+    plt.hlines(y=-2, xmin=np.min(df['y']), xmax=np.max(df['y']), color='red', ls='dashed')
+    plt.title(y_hat)
+
+def plot_y_vs_y_hat(df, y_hat):
+    sns.scatterplot(data=df,
+                    x='y',
+                    y=y_hat,
+                    hue='perturbation',
+                    #s=15,
+                    alpha=0.5)
+    plt.axis('equal')
+
+    min_val = df[['y', y_hat]].min().values.min()
+    max_val = df[['y', y_hat]].max().values.min()
+
+    x_line = [min_val, max_val]
+    y_line = x_line
+    plt.plot(x_line, y_line, linestyle='dashed', color='red')
+    plt.title(y_hat)
+
+def get_BIC(y, y_hat, k):
+    if k > 0:
+        SS_residual = np.sum((y-y_hat)**2)
+        n = y.shape[0]
+
+        return n * np.log(SS_residual / n) + np.log(n) * k
+    else:
+        return 0
+
+def get_metrics(df=None, k=0):
+    huber_loss = tf.keras.losses.Huber()
+
+    # Create df to store metrics to compare models
+    metric_columns = ['model', 'set', 'R2', 'BIC', 'MSE', 'MAE', 'Huber']
+    temp_df = pd.DataFrame(columns=metric_columns)
+
+    for ss in np.unique(df['set']):
+        mask = (df['set'] == ss)
+        y = df['y'][mask].values
+        y_hat = df['y_hat'][mask].values
+        r2 = r2_score(y, y_hat)
+        bic = get_BIC(y=y, y_hat=y_hat, k=k)
+        mse = mean_squared_error(y, y_hat)
+        mae = mean_absolute_error(y, y_hat)
+        huber = huber_loss(y, y_hat).numpy()
+        temp_df = temp_df.append({'model':'y_hat', 'set':ss, 'R2':r2, 'BIC':bic, 'MSE':mse, 'MAE':mae, 'Huber':huber}, ignore_index=True)
+
+    return temp_df.round(4)
+
+def plot_loss(history, metrics, p):
+    keys = ['loss'] + metrics
+    for i, key in enumerate(keys,1):
+        warm_stage = int(p['number_of_epochs']*0.20)
+        min_val = np.asarray(history[key]+history['val_'+key]).min()
+        max_val = np.asarray(history[key][warm_stage:]+history['val_'+key][warm_stage:]).max()
+
+        plt.subplot(3,1,i)
+        plt.plot(history[key], label=key)
+        plt.plot(history['val_'+key], label='val_'+key)
+        val_min = np.asarray(history['val_'+key]).min()
+        val_min_idx = np.argmin(history['val_'+key])
+        label='bets val value\nEpoch={}\n{}={}'.format(val_min_idx,key,round(val_min,2))
+        plt.scatter(x=val_min_idx, y=val_min, c='red', linewidths=4, label=label)
+        plt.grid(True)
+        plt.ylim([min_val, max_val])
+        plt.xlabel('Epoch')
+        plt.ylabel(key)
+        plt.legend()
+        plt.title(key)
