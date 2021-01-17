35a2c94f16774fe222fb3b43098b38791f43f647
Andres Becker <abecker.esfm@gmail.com>
2021-01-17 16:15:27 +0100

    now the normalization and split (train, val, test) is done during the creation of the TFDS

--- workspace/tf_datasets_scripts/MPP_dataset_Normal_DMSO_G1/MPP_dataset_Normal_DMSO_G1.py
+++ workspace/tf_datasets_scripts/MPP_dataset_Normal_DMSO_G1/MPP_dataset_Normal_DMSO_G1.py
@@ -6,17 +6,18 @@ import numpy as np
 import tensorflow as tf
 import os
 import json
+import sys
 
 _DESCRIPTION = """
 Dataset containing images of Multiplexed protein maps.
 The elements of this data sets are multichannel images of singel cells alongside with its transcription rate.
-This Dataset was builded after a preprocessing using the python script Transform_MPPData_into_images_from_script.ipynb. During this preprocessing the original MPPData was:
+This Dataset was builded after a preprocessing using the python script Transform_MPPData_into_images_no_split_from_script.ipynb. During this preprocessing the original MPPData was:
 - Converted into images.
 - Cleaned. Border and mitotic cells were removed.
 - Normalized. Each channel was normalized using scale parameters obtained from the training set.
 - Target value (scalar) calculated. The transcription rate was approximated taking the average of the measured pixels of the channel 00_EU. It is important to mention that the the target value was calculated BEFORE the normalization process.
 
-This Dataset contains only the cells with no perturbations (i.e. cells such that perturbation in ['normal', 'DMSO']). Although perturbations 'TSA' seams not to have influence over the TR (00_EU avg), it seams to have an influence on the intensity of the channel 10_H3K27ac, and therefore it is not included. Besides, this dataset only contain cell in G1 state.
+This Dataset contains only the cells with no perturbations (i.e. cells such that perturbation in ['normal', 'DMSO']). Although perturbations 'TSA' seams not to have influence over the TR (00_EU avg), it seams to have an influence on the intensity of the channel 10_H3K27ac, and therefore it is not included. Also, this dataset contain only cell in G1 state.
 
 """
 
@@ -59,13 +60,15 @@ class MPP_dataset_Normal_DMSO_G1(tfds.core.GeneratorBasedBuilder):
 		# Get channel ids corresponding to the input and the output
 		self.input_ids, self.output_id = self._get_IO_channel_ids()
 
+		# Get normalization parameters
+		self.normalization_vals = self._get_normalization_values()
+
 		# Save output of this script
 		self._save_tfds_metadata()
 
 		# To make return more readable
 		img_size = self.pp_param['img_size']
 		n_channels = self.input_ids.shape[0]
-		img_dtype = getattr(tf, self.pp_param['images_dtype'])
 
 		return tfds.core.DatasetInfo(
 			builder=self,
@@ -73,7 +76,7 @@ class MPP_dataset_Normal_DMSO_G1(tfds.core.GeneratorBasedBuilder):
 			features=tfds.features.FeaturesDict({
 				# These are the features of your dataset like images, labels ...
 				'mapobject_id_cell': tfds.features.Text(),
-				'image': tfds.features.Tensor(shape=(img_size, img_size, n_channels), dtype=img_dtype),
+				'image': tfds.features.Tensor(shape=(img_size, img_size, n_channels), dtype=tf.float32),
 				'target': tfds.features.Tensor(shape=(1,), dtype=tf.float64),
 				}),
 
@@ -115,11 +118,21 @@ class MPP_dataset_Normal_DMSO_G1(tfds.core.GeneratorBasedBuilder):
 		file_names = [fn for fn in file_names if fn[:-4] in filtered_cells]
 		file_names = np.array(file_names)
 
+		self.normalization_vals = self.normalization_vals.astype(np.float32)
+		n_channels = self.input_ids.shape[0]
+		img_size = self.pp_param['img_size']
+		img_shape = (img_size, img_size, n_channels)
+
 		for fn in file_names:
 			cell = np.load(images_path.joinpath(fn))
 			cell_id = int(fn.split('.')[0])
 			cell_img = cell['img'][:,:,self.input_ids]
 			cell_target = [cell['targets'][self.output_id]]
+			# Normalize cell image
+			cell_img = cell_img.astype(np.float32)
+			cell_img = cell_img.reshape((-1, n_channels))
+			cell_img /= self.normalization_vals
+			cell_img = cell_img.reshape(img_shape)
 
 			yield cell_id, {
 				'mapobject_id_cell': str(cell_id),
@@ -208,6 +221,62 @@ class MPP_dataset_Normal_DMSO_G1(tfds.core.GeneratorBasedBuilder):
 
 		return filtered_metadata
 
+	def _get_normalization_values(self):
+		"""
+		Get the normalization rescale values for each selected input channel from the train partition and the given percentile.
+	    Input:
+	        input_channel_ids: list indicating for which channels we must calculate the normalization parameters.
+	        percentile: integer in [0,100].
+	    Output:
+	        norm_vals: numpy array of lenght number_of_channels, containing the normalization values of each channel
+		"""
+		def apply_mask_to_channel(channel):
+			return channel[cell_mask]
+
+		cell_path = os.path.join(self.data_source_path, 'data')
+		# Get file names
+		file_names = os.listdir(cell_path)
+		file_names = [name for name in file_names if name[-4:] == '.npz']
+
+		# take imaes that are in the file system, the filtered_metadata df and
+		# correspond to the train subset
+		mask = (self.filtered_metadata.set == 'train')
+		filtered_cells = self.filtered_metadata.mapobject_id_cell[mask].values.astype(str)
+		file_names = [fn for fn in file_names if fn[:-4] in filtered_cells]
+		file_names = np.array(file_names)
+
+	    # The percentile per channel need to be taken accordingly to the mask. Since there is no way to concatenate np arrays in place (without creating a copy), we first create an array that will be filled with the values necessary to calculate the percentile per channel. Note that the size of this array is the same for every channel.
+	    #https://stackoverflow.com/questions/7869095/concatenate-numpy-arrays-without-copying
+		n_pixels = 0
+		for fn in file_names:
+			cell = np.load(os.path.join(cell_path, fn))
+			n_pixels += cell['mask'].sum()
+		print('Number of pixels for each channel and for all the training cells images:{}'.format(n_pixels))
+
+		# Create the array that will hold the train dataset filtered by the mask
+		n_channels = self.input_ids.shape[0]
+		img_dtype = getattr(np, self.pp_param['images_dtype'])
+		train_pixels = np.zeros((n_pixels, n_channels), dtype=img_dtype)
+		print('Size of the array that holds the training pixels (GB): {}'.format(sys.getsizeof(train_pixels)/1e9))
+
+		# Fill train_pixels with the train data
+		idx_l = 0
+		idx_h = 0
+		for fn in file_names:
+			cell = np.load(os.path.join(cell_path, fn))
+			cell_mask = cell['mask'].reshape(-1)
+			cell_img = cell['img'][:,:,self.input_ids]
+			cell_img = cell_img.reshape((-1, n_channels))
+			idx_l = idx_h
+			idx_h += cell_mask.sum()
+
+			train_pixels[idx_l:idx_h,:] = np.apply_along_axis(apply_mask_to_channel, 0, cell_img)
+
+		norm_vals = np.percentile(train_pixels, self.tfds_param['percentile'], axis=0)
+
+		return norm_vals
+
+
 	def _get_IO_channel_ids(self):
 
 		# Input ids
@@ -232,7 +301,8 @@ class MPP_dataset_Normal_DMSO_G1(tfds.core.GeneratorBasedBuilder):
 		global _DESCRIPTION
 		_DESCRIPTION += '\ninput_channels:\n{}'.format(self.tfds_param['input_channels'])
 		_DESCRIPTION += '\n\noutput_channel:\n{}'.format(self.tfds_param['output_channels'])
-		_DESCRIPTION += '\n\nTFDS metadata path:\n{}'.format(script_output_path)
+		_DESCRIPTION += '\n\nNormalization values:\n{}'.format(self.normalization_vals)
+		_DESCRIPTION += '\n\nMore information about this TFDS (metadata, metadata after filtering, dataset creation parameters and channel info including normalization values) can be found on:\n{}\n\n'.format(script_output_path)
 
 		# save original metadata:
 		with open(os.path.join(script_output_path, 'metadata.csv'), 'w') as file:
@@ -242,10 +312,6 @@ class MPP_dataset_Normal_DMSO_G1(tfds.core.GeneratorBasedBuilder):
 		with open(os.path.join(script_output_path, 'filtered_metadata.csv'), 'w') as file:
 			self.filtered_metadata.to_csv(file, index=False, sep=',')
 
-		# Save channel file:
-		with open(os.path.join(script_output_path, 'channels.csv'), 'w') as file:
-			self.channels.to_csv(file, index=False, sep=',')
-
 		# save tfds creation parameters
 		with open(os.path.join(script_output_path, 'tfds_parameters.json'), 'w') as file:
 			json.dump(self.tfds_param, file, indent=4)
@@ -253,3 +319,21 @@ class MPP_dataset_Normal_DMSO_G1(tfds.core.GeneratorBasedBuilder):
 		# save data preprocessing parameters
 		with open(os.path.join(script_output_path, 'data_pp_parameters.json'), 'w') as file:
 			json.dump(self.pp_param, file, indent=4)
+
+		# Save channel file:
+		# First add the normalization values to the output channel file
+		norm_vals_df = pd.DataFrame(self.input_ids, columns=['channel_id'])
+		norm_vals_df['normalization_vals'] = self.normalization_vals
+		norm_vals_df['type'] = 'input'
+
+		mask = self.channels.channel_id.isin(self.input_ids)
+		self.channels = self.channels[mask]
+		self.channels = self.channels.merge(norm_vals_df,
+											left_on='channel_id',
+											right_on='channel_id',
+											how='left')
+		temp_dict = {'channel_id':self.output_id, 'name':(self.tfds_param['output_channels'])[0], 'normalization_vals':1, 'type':'output'}
+		self.channels = self.channels.append(temp_dict, ignore_index=True)
+
+		with open(os.path.join(script_output_path, 'channels.csv'), 'w') as file:
+			self.channels.to_csv(file, index=False, sep=',')
