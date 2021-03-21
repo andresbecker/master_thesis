d09104f0cee7c437dd31eecd61f4117ac36f75a7
Andres Becker <abecker.esfm@gmail.com>
2021-03-21 12:17:50 +0100

    format in random noise params display corrected

--- workspace/libs/Utils.py
+++ workspace/libs/Utils.py
@@ -701,13 +701,13 @@ def set_model_default_parameters(p_old=None):
         p_new[key] = False
     else:
         p_new[key] = p_old[key]
-    info += '\n    '+key+str(p_new[key])
+    info += '\n    '+key+': '+str(p_new[key])
     key = 'Random_noise_stddev'
     if key not in p_old.keys():
         p_new[key] = 1
     else:
         p_new[key] = p_old[key]
-    info += '\n    '+key+str(p_new[key])
+    info += '\n        '+key+': '+str(p_new[key])
 
     info += '\n'
     # End of the Data augmentation section--------------------------------------
